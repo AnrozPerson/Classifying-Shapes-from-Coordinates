11adc85cab675d439470e43d125116f8007f5930
Arun Rajayogan <arun.rajayogan@student.education.wa.edu.au>
2023-09-11 18:56:03 +0800

    Added skeleton function for finding angle length Imported numpy to make math in python easier, such as (easier) access to trig functions, some nicer looking math functions etc. Began to outline the user input component of the code, will store these vertices in a list of tuples for easy access.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 ###Main.py
 
-#Import Matplot library to do further computation if needed
+#Import Matplot library to do visual graph things
+#Import numpy for math functions
 import matplotlib.pyplot as plt
+import numpy as np
 
+shape = []
+
+#a and b will be tuples holding coordinates
+def side_length(a, b):
+    pass
+
+
+
+
+#User Input
